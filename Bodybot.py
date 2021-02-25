ab38dfdbfab09799fee8128a93bb89d54932ae78
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-02-25 21:02:42 +0300

    Add files via upload

---
+++ Bodybot.py
@@ -0,0 +1,34 @@
+import telebot
+
+
+with open('D:\\Python\\_1_\\_bot\\secrets.py') as inf:
+    a = inf.read()
+
+
+bot = telebot.TeleBot(a)
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    print(message.from_user)
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def sent_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'До встречи, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAM0YDaQ24SzhzkWTzYu8pVRCblOTrgAAgEAA8A2TxMYLnMwqz8tUR4E')
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+
+bot.polling()
