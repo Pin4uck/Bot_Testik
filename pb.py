925ca55095302fbe889e8824c71b99a438c0eba9
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-04-16 13:37:57 +0300

    добавляем try - except
     добавляем try - except на случай не заполнения массива

--- pb.py
+++ pb.py
@@ -20,7 +20,10 @@ def get_exchange(ccy_key):
 def get_exchanges(ccy_pattern):
     result = []
     ccy_pattern = re.escape(ccy_pattern) + '.*'
-    for exc in load_exchange():
-        if re.match(ccy_pattern, exc['Cur_Abbreviation'], re.IGNORECASE) is not None:
-            result.append(exc)
-    return result[0]['Cur_ID'], result[0]['Cur_Abbreviation'], result[0]['Cur_OfficialRate']
+    try:
+        for exc in load_exchange():
+            if re.match(ccy_pattern, exc['Cur_Abbreviation'], re.IGNORECASE) is not None:
+                result.append(exc)
+        return result[0]['Cur_ID'], result[0]['Cur_Abbreviation'], result[0]['Cur_OfficialRate']
+    except:
+        return 'не удалось заполнить массив' 
