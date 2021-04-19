8c10247f75e3001d41cd03768e46005ba7dfb40f
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-04-19 14:38:52 +0300

    Исправление ошибки в dbcursor()
    И исправление других мелких ошибок

--- currency.py
+++ currency.py
@@ -3,6 +3,7 @@ import pb
 import os
 import logging
 
+dbconn = None
 CACHE_AGE = int(os.getenv('TGBOT_CACHE_MAXAGE', 3600))
 LOG_LEVEL = os.getenv("TGBOT_LOGLEVEL", "WARNING")
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=LOG_LEVEL)
@@ -26,35 +27,37 @@ def get_currency_rate2(currency_name):
 
 
 def dbcursor():
-    dbconn = None
+    global dbconn
     if not dbconn:
         dbconn = sqlite3.connect('currency.db')
         dbconn.execute('''CREATE TABLE IF NOT EXISTS ccy (
                             Abbreviation TEXT NOT NULL PRIMARY KEY,
                             Rate FLOAT NOT NULL,
-                            last_updated timestamp default (strftime('%s', 'now')))'''
-        )
+                            last_updated timestamp default (strftime('%s', 'now')))''')
     return dbconn.cursor(), dbconn
 
 
 def read_cached_currency_rate(currency_name, cache = None):
-    curs, dbconn = dbcursor()
-    if cache == None:
+    curs, _ = dbcursor()
+    if cache is None:
         curs.execute(f'''SELECT Rate FROM ccy 
                                 WHERE Abbreviation = '{currency_name}' ''')
         logging.debug('not updated value from base')
+        value = curs.fetchall()
     else:
         curs.execute(f'''SELECT Rate FROM ccy 
                         WHERE Abbreviation = '{currency_name}' and (strftime('%s') - last_updated <= {cache})''')
+        value = curs.fetchall()
+        if value:
+            logging.debug('updated value from base')
     value = curs.fetchall()
     if value:
-        logging.debug('updated value from base')
         return value[0][0]
     raise LookupError
 
     
 def store_currency_rate(currency_name, rate):
-    curs, dbconn = dbcursor()
+    curs, conn = dbcursor()
     curs.execute(f'''INSERT INTO ccy(Abbreviation, Rate, last_updated) VALUES('{currency_name}', {rate}, strftime("%s"))  
                         on conflict (Abbreviation) do update set Rate=excluded.Rate, last_updated = excluded.last_updated''')
-    dbconn.commit()
+    conn.commit()
