227112b526f72a2dcf176bf6780ab22d57ad0943
Pin4uck <79600533+Pin4uck@users.noreply.github.com>
2021-04-21 19:45:39 +0300

    Внесены поправки pylint в файл pb.py

--- pb.py
+++ pb.py
@@ -1,16 +1,20 @@
+"""Модуль json позволяет кодировать и декодировать данные в удобном формате"""
+import json
 import re
 import requests
-import json
 
 
 URL = 'https://www.nbrb.by/api/exrates/rates?periodicity=0'
 
 
 def load_exchange():
+    """Данная функция возвращает полученные данные с сайта"""
     return json.loads(requests.get(URL).text)
 
 
 def get_exchange(ccy_key):
+    """Данная функция возвращает данные по валюте(ccy_key)
+     из полученных данных в load_exchange()"""
     for exc in load_exchange():
         if ccy_key == exc['Cur_Abbreviation']:
             return exc
@@ -18,6 +22,8 @@ def get_exchange(ccy_key):
 
 
 def get_exchanges(ccy_pattern):
+    """Данная функция возвращает курс валют(exc['Cur_OfficialRate'])
+     из полученных данных в get_exchange(ccy_key)"""
     ccy_pattern = re.escape(ccy_pattern) + '.*'
     for exc in load_exchange():
         if re.match(ccy_pattern, exc['Cur_Abbreviation'], re.IGNORECASE) is not None:
